08f033bc971486cdb22b4cb499d7f95dda30670b
Yevheniy Oliynyk <evgen41293@gmail.com>
2018-05-08 12:58:12 +0300

    Fixed template method module, pushed to github

---
+++ design-patterns/template-method/src/main/java/org/evgen/ua/Launch.java
@@ -0,0 +1,9 @@
+package org.evgen.ua;
+
+public class Launch {
+
+  public static void main(String[] args) {
+    Shape circle = new Circle();
+    circle.build();
+  }
+}
--- enterprise-patterns/transaction-script/src/main/java/org/evgen/ua/Shop.java
+++ enterprise-patterns/transaction-script/src/main/java/org/evgen/ua/Shop.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 public class Shop {
 
     public void addNewProduct(String name, BigDecimal price) {
-        //here should be real logic but in one place
+        //here should be real logic but all in one place
         System.out.println("Saving new product " + name + " for price " + price);
         System.out.println("Loading all client in order to send them notification");
         System.out.println("Sending notification to clients about new product");
